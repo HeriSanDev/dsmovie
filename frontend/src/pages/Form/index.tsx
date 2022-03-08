ca3ecdeea075e5a71ab93b14f0daf94012755177
HeriSanDev <herisilvasantos@outlook.com>
2022-03-08 03:04:56 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom';
 import './styles.css'
 
 function Form() {
@@ -32,10 +33,14 @@ function Form() {
                     </select>
                 </div>
                 <div className="dsmovie-form-btn-container">
+                  
                     <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
+                
                 </div>
             </form >
+            <Link to="/">
             <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+            </Link>
         </div >
     </div >
     );
